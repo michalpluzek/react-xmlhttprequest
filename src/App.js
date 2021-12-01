1bed1c80dafed4891f2a327926f83bc94bb69f28
Michał Płużek <michalpluzek@gmail.com>
2021-12-01 11:43:44 +0100

    Refactoring to hooks

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class App extends React.Component {
-  state = {
-    users: [],
-  };
+const App = () => {
+  const [users, setUsers] = useState([]);
 
-  baseURL = "https://jsonplaceholder.typicode.com/users";
+  const baseURL = "https://jsonplaceholder.typicode.com/users";
 
-  componentDidMount() {
+  useEffect(() => {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", this.baseURL, true);
+    xhr.open("GET", baseURL, true);
     // xhr.onload = () => {
     //   if (xhr.status === 200) {
     //     const users = JSON.parse(xhr.response);
 
-    //     this.setState({
-    //       users,
-    //     });
+    //     setUsers(users)
     //   }
     // };
 
-    xhr.addEventListener("load", () => {
-      if (xhr.status === 200) {
-        const users = JSON.parse(xhr.response);
-
-        this.setState({
-          users,
-        });
-      }
-    });
+    xhr.addEventListener("load", () => onLoad(xhr));
 
     xhr.send();
-  }
+    return () => xhr.removeEventListener("load", onLoad);
+  }, []);
 
-  render() {
-    const users = this.state.users.map((user) => (
-      <div key={user.id}>
-        <h3>{user.name}</h3>
-        <p>{user.address.street}</p>
-      </div>
-    ));
-    return <div>{users}</div>;
-  }
-}
+  const onLoad = (xhr) => {
+    if (xhr.status === 200) {
+      const users = JSON.parse(xhr.response);
+
+      setUsers(users);
+    }
+  };
+
+  const _users = users.map((user) => (
+    <div key={user.id}>
+      <h3>{user.name}</h3>
+      <p>{user.address.street}</p>
+    </div>
+  ));
+
+  return <div>{_users}</div>;
+};
 
 export default App;
